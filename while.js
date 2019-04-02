bac11eba6fb85bb539d56b669bd153cee7cb5af3
Minh <minhdotcom@gmail.com>
2019-04-02 10:48:58 +0700

    done

--- while.js
+++ while.js
@@ -2,6 +2,9 @@ let number = 0;
 let sum = 0;
 while (number != -1) {
     number = Number(prompt('Nhap so bat ky:'))
+    if(number == -1) {
+        break;
+    }
     document.write(number + '<br>')
     sum += number;
 }
